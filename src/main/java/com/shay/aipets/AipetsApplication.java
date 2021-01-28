41d604a18413413ba307a9a1544ae59d6cdc1b12
tukinokage <3355847539@qq.ccom>
2021-01-29 00:58:34 +0800

    Initial commit

---
+++ src/main/java/com/shay/aipets/AipetsApplication.java
@@ -0,0 +1,13 @@
+package com.shay.aipets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AipetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shay/aipets/AipetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shay.aipets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
